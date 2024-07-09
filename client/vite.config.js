8d8aa857b95035d64580fdb51d005e99b316aa12
Anupam Nandi <116145439+Anupam-03@users.noreply.github.com>
2024-07-09 21:13:23 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    outDir: 'dist', // Specify the output directory for the build artifacts
+    minify: 'esbuild', // Use esbuild for faster minification
+    sourcemap: true, // Enable source maps for easier debugging
+    rollupOptions: {
+      input: '/index.html', // Specify the entry point for your app
+    },
+  },
 })
